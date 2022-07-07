6980322e374404c492d108c630bc4035c45f9e1e
mdsabbirkhan1371 <mdsabbirkhan1972@gmail.com>
2022-07-08 01:24:02 +0800

    how many items i used i added

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
+# How many things i used in this projecet.
+I used in this project react js,react router,react-authentication-hooks,react-form-hook,react  bootstrap,firebase authentication, express js,mongodb.
 ## Available Scripts
 
 In the project directory, you can run:
--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,9 +2,27 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <div>
+        <div className='p-5 border border-5 border-primary'>
+            <div className='border p-3 mb-2 border-3 border-success'>
+                <h2>1: What is  different between Javascript and node.js??</h2>
+                <p>JavaScript is a simple programming language that runs in any browser in JavaScript Engine. Whereas Node JS is an interpreter or running environment for a JavaScript programming language that holds many excesses, it requires libraries that can easily be accessed from JavaScript programming for better use.</p>
+            </div>
+            <div className='border p-3 mb-2 border-3 border-success'>
+                <h2>2: When should you use nodejs and when should you use mongodb</h2>
+                <p>There are many web servers built with node.js that will use in MongoDB for storing data. MongoDB offers an API library that runs with in a Nodejs application to give you programmatic access to MongoDB so you can create databases and then add, query, update or delete data from the MongoDB database</p>
+            </div>
+            <div className='border p-3 mb-2 border-3 border-success'>
+                <h2>3. Differences between sql and nosql databases?</h2>
+                <p>Sql data base is vertically scalable while Nosql are horizontally scalable.Sql are table based data while NoSQL databases are document, key-value, graph, or wide-column stores. SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON. </p>
+            </div>
+            <div className='border p-3 mb-2 border-3 border-success'>
+                <h2>4.What is the purpose of jwt and how does it work?</h2>
+                <p>Jwt or json web token is an open standard used to shared security information between   client side or server side. Each JWT contains encoded JSON objects, including a set of claims. JWTs are signed using a cryptographic algorithm to ensure that the claims cannot be altered after the token is issued.</p>
+            </div>
 
         </div>
+
+
     );
 };
 
